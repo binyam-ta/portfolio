388cbd318c33a2ba4e210408867cc63c818f91ad
binyam-ta <binyamseleshe12@gmail.com>
2024-10-16 18:03:39 +0300

    header

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+import memojiImage from '@/assets/images/memoji-computer.png'
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+  <div>
+   <div className="container">
+    <Image src={memojiImage} alt="person peeking from behind laptop"/>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+   </div>
+  </div>);
 };
