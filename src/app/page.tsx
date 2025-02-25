dc209c9559197b2760048bb7100b557f05430a03
binyam-ta <binyamseleshe12@gmail.com>
2025-02-25 17:41:34 +0300

    unescaped-entities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {Header} from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
@@ -6,7 +7,20 @@ import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
+import LogRocket from 'logrocket';
+
+
+
+// import { useEffect } from "react";
 export default function Home() {
+  if (process.env.NODE_ENV === 'production') {
+    LogRocket.init('YOUR_APP_ID'); // Replace 'YOUR_APP_ID' with your actual LogRocket app ID
+  }
+  // useEffect(() =>{
+  //   if (process.env.NODE_ENV ===  'production') {
+  //     LogRocket.init('YOUR_APP_ID');
+  //   }
+  // }, []);
   return (
     <div>
       <Header/>
